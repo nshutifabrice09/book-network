42e004b284fed56fd83c037b3625112679d422d1
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-12-03 12:35:37 +0200

    final repo updates

---
+++ src/main/java/com/backend/book_network/model/Role.java
@@ -0,0 +1,38 @@
+package com.backend.book_network.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdTime;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+}
---
+++ src/main/java/com/backend/book_network/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.backend.book_network.repository;
+
+import com.backend.book_network.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findUserByEmail(String email);
+}

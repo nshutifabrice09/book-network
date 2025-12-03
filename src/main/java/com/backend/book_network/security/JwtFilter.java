0020ebe5210349c51f5f562f6b59b5c6c82c72c5
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-12-03 23:16:18 +0200

    decoding secretKey

--- src/main/java/com/backend/book_network/security/JwtFilter.java
+++ src/main/java/com/backend/book_network/security/JwtFilter.java
@@ -1,22 +1,46 @@
 package com.backend.book_network.security;
 
-import io.jsonwebtoken.io.IOException;
+import com.backend.book_network.service.JwtService;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+
 @Service
+@RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+//    public JwtFilter(JwtService jwtService) {
+//        this.jwtService = jwtService;
+//    }
 
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
-            ) throws ServletException, IOException {
-
+        ) throws ServletException, IOException {
+        if (request.getServletPath().contains("/api/v1/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String jwt;
+        final String userEmail;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
     }
 }

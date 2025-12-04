8d805ca650af8c0f903a0ef607d23fe06ab87930
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-12-04 14:36:57 +0200

    implementing generateActivationToken

--- src/main/java/com/backend/book_network/controller/RegistrationRequest.java
+++ src/main/java/com/backend/book_network/controller/RegistrationRequest.java
@@ -28,35 +28,4 @@ public class RegistrationRequest {
     @Size(min = 8, message = "Password should be 8 characters long minimum")
     private String password;
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }

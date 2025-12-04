53bd84e98c6a03ba4391ea6893fd3f2c75938017
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-12-04 13:15:41 +0200

    Initial AuthenticationController

---
+++ src/main/java/com/backend/book_network/controller/AuthenticationController.java
@@ -0,0 +1,9 @@
+package com.backend.book_network.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+// /api/auth/login, /register
+@RestController
+public class AuthenticationController {
+
+}

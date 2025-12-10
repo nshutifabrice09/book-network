1b45428caa80427ed2f24da0c7b16bb278f3ca61
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-12-10 17:20:48 +0200

    Initial GlobalExpetionHandler

---
+++ src/main/java/com/backend/book_network/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.backend.book_network.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}

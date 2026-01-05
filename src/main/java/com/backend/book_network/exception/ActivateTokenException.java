a1569b858a5b6eae5e64abe66a16875092146a16
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2026-01-05 14:07:23 +0200

    additional exceptions

---
+++ src/main/java/com/backend/book_network/exception/ActivateTokenException.java
@@ -0,0 +1,9 @@
+package com.backend.book_network.exception;
+
+public class ActivateTokenException extends RuntimeException{
+
+    public ActivateTokenException(String message) {
+        super(message);
+    }
+
+}

43e238cc91f05a35729f983d59d58735439d9f6c
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2026-01-05 10:42:31 +0200

    applicationAuditAware

---
+++ src/main/java/com/backend/book_network/exception/ExceptionResponse.java
@@ -0,0 +1,22 @@
+package com.backend.book_network.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)  //to include non-empty attributes
+public class ExceptionResponse {
+
+    private Integer businessErrorCode;
+    private String businessErrorDescription;
+    private String error;
+    private Set<String> validationErrors;
+    private Map<String, String> errors;
+}
---
+++ src/main/java/com/backend/book_network/model/BusinessErrorCodes.java
@@ -0,0 +1,29 @@
+package com.backend.book_network.model;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+public enum BusinessErrorCodes {
+
+    NO_CODE(0, NOT_IMPLEMENTED, "No codes."),
+    INCORRECT_CURRENT_PASSWORD(300, BAD_REQUEST, "Current password is incorrect."),
+    NEW_PASSWORD_DOES_NOT_MATCH(300, BAD_REQUEST, "New password does not match."),
+    ACCOUNT_LOCKED(302, FORBIDDEN, "User account is locked."),
+    ACCOUNT_DISABLED(302, FORBIDDEN, "User account is disabled."),
+    BAD_CREDENTIALS(304, FORBIDDEN, "Login and / or password is incorrect.");
+
+    @Getter
+    private final int code;
+    @Getter
+    private final HttpStatus httpStatus;
+    @Getter
+    private final String description;
+
+    BusinessErrorCodes(int code, HttpStatus httpStatus, String description) {
+        this.code = code;
+        this.httpStatus = httpStatus;
+        this.description = description;
+    }
+}
